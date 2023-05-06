2a96f098b9536f09ad39ca2a9e13ba213a576f78
AmyGois <ahagois@hotmail.com>
2023-05-06 14:23:09 +0100

    Create basic Book constructor & library

---
+++ script.js
@@ -0,0 +1,15 @@
+const myLibrary = [];
+
+function Book(title, author, year, edition, publisher, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.year = year;
+  this.edition = edition;
+  this.publisher = publisher;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBookToLibrary() {
+  // Add new book to array from user input
+}
